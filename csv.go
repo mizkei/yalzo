76375e21c08ce36fd20d455ffeab78876c19965e
tkyshm <tkyshm@gmail.com>
2015-10-04 02:39:39 +0900

    fmtかけわすれ　

--- csv.go
+++ csv.go
@@ -1,32 +1,31 @@
 package yalzo
 
 import (
-    "os"
-    "bufio"
-    "strconv"
-    "strings"
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func ReadCSV(fp *os.File) ([]Todo,error) {
-    scanner := bufio.NewScanner(fp)
-    todos := make([]Todo, 0, 100)
+func ReadCSV(fp *os.File) ([]Todo, error) {
+	scanner := bufio.NewScanner(fp)
+	todos := make([]Todo, 0, 100)
 
-    for scanner.Scan() {
-        line := scanner.Text() 
-        items := strings.Split(line, ",")
+	for scanner.Scan() {
+		line := scanner.Text()
+		items := strings.Split(line, ",")
 
-        no, err := strconv.Atoi(strings.TrimSpace(items[0]))
-        if err != nil {
-            return nil, err
-        }
+		no, err := strconv.Atoi(strings.TrimSpace(items[0]))
+		if err != nil {
+			return nil, err
+		}
 
-        todo := &Todo{ 
-            no:    no,
-            label: strings.TrimSpace(items[1]),
-            title: strings.TrimSpace(items[2]),
-        }
-        todos = append(todos, (*todo)) 
-    }
-    return todos,nil
+		todo := &Todo{
+			no:    no,
+			label: strings.TrimSpace(items[1]),
+			title: strings.TrimSpace(items[2]),
+		}
+		todos = append(todos, (*todo))
+	}
+	return todos, nil
 }
-
