9cc6c8f4fd528c68beaa86d7e072ef558d02dbf5
tkyshm <tkyshm@gmail.com>
2015-10-04 10:32:49 +0900

    足りない諸々のAPIの修正および追加 TODOおよびArchiveへの移動、archiveフラグの追加、全TODOリストの表示用リスト

--- todo.go
+++ todo.go
@@ -2,10 +2,12 @@ package yalzo
 
 import (
 	"os"
+	"strconv"
 )
 
 type TodoList struct {
-	list   []Todo
+	todos  []Todo
+	archs  []Todo
 	labels []string
 }
 
@@ -16,43 +18,100 @@ type Todo struct {
 	no         int
 }
 
+type Tab int
+
+const (
+	TODO Tab = iota
+	ARCHIVE
+)
+
 func NewTodoList(fp *os.File, ls []string) *TodoList {
-	l, err := ReadCSV(fp)
+	l, as, err := ReadCSV(fp)
 	if err != nil {
 		panic(err)
 	}
+
 	return &TodoList{
-		list:   l,
+		todos:  l,
+		archs:  as,
 		labels: ls,
 	}
 }
 
+func (tl *TodoList) GetTodos(width int) []string {
+	lines := make([]string, 0, 100)
+	for i := 0; i < len(tl.todos); i++ {
+		lines = append(lines, tl.todos[i].tolimitStr(width))
+	}
+	return lines
+}
+
+func (tl *TodoList) GetArchives(width int) []string {
+	lines := make([]string, 0, 100)
+	for i := 0; i < len(tl.archs); i++ {
+		lines = append(lines, tl.archs[i].tolimitStr(width))
+	}
+	return lines
+}
+
 func (tl *TodoList) ChangeLabelName(i int, l string) {
-	(*tl).list[i].label = l
+	(*tl).todos[i].label = l
 }
 
 func (tl *TodoList) Delete(n int) {
-	for i := n; i < len((*tl).list); i++ {
-		(*tl).list[i].setNumber(i)
+	for i := n; i < len((*tl).todos); i++ {
+		(*tl).todos[i].setNumber(i)
 	}
-	tl.list = append(tl.list[:n], tl.list[n+1:]...)
+	tl.todos = append(tl.todos[:n], tl.todos[n+1:]...)
 }
 
 func (tl *TodoList) AddTodo(l string, t string) {
-	tl.list = append(tl.list, Todo{
-		no:    len(tl.list),
+	tl.todos = append(tl.todos, Todo{
+		no:    len(tl.todos),
 		label: l,
 		title: t,
 	})
 }
 
-func (tl *TodoList) Exchange(i1 int, i2 int) {
-	(*tl).list[i2].setNumber(i1 + 1)
-	(*tl).list[i1].setNumber(i2 + 1)
+func (tl *TodoList) MoveArchive(n int) {
+	length := len(tl.todos)
+	tl.todos[n].isArchived = true
+	tl.todos[n].setNumber(length)
+	tl.archs = append(tl.archs, tl.todos[n])
+	for i := n + 1; i < len(tl.todos); i++ {
+		tl.todos[i].setNumber(i - 1)
+	}
+	tl.todos = append(tl.todos[:n], tl.todos[n+1:]...)
+}
 
-    (*tl).list[i2], (*tl).list[i1] = (*tl).list[i1], (*tl).list[i2] 
+func (tl *TodoList) MoveTodo(n int) {
+	length := len(tl.todos)
+	tl.archs[n].isArchived = false
+	tl.archs[n].setNumber(length)
+	tl.todos = append(tl.todos, tl.archs[n])
+	for i := n + 1; i < length; i++ {
+		tl.todos[i].setNumber(i - 1)
+	}
+	tl.archs = append(tl.archs[:n], tl.archs[n+1:]...)
+}
+
+func (tl *TodoList) Exchange(i1 int, i2 int) {
+	(*tl).todos[i2].setNumber(i1 + 1)
+	(*tl).todos[i1].setNumber(i2 + 1)
+
+	(*tl).todos[i2], (*tl).todos[i1] = (*tl).todos[i1], (*tl).todos[i2]
 }
 
 func (t *Todo) setNumber(n int) {
 	(*t).no = n
+}
+
+func (t *Todo) tolimitStr(limit int) string {
+	str := strconv.Itoa(t.no) + t.label + t.title
+	length := len(str)
+	if length > limit {
+		return str[:limit]
+	} else {
+		return str
+	}
 }
