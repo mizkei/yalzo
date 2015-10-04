367782266ffe844bbd8e70d55fc45db9824600cd
tkyshm <tkyshm@gmail.com>
2015-10-04 18:33:49 +0900

    SaveCSVの実装

--- csv.go
+++ csv.go
@@ -2,13 +2,14 @@ package yalzo
 
 import (
 	"bufio"
-	"io"
+	"bytes"
+	"os"
 	"strconv"
 	"strings"
 )
 
-func ReadCSV(r io.Reader) ([]Todo, []Todo, error) {
-	scanner := bufio.NewScanner(r)
+func ReadCSV(fp *os.File) ([]Todo, []Todo, error) {
+	scanner := bufio.NewScanner(fp)
 	if err := scanner.Err(); err != nil {
 		return nil, nil, err
 	}
@@ -47,6 +48,35 @@ func ReadCSV(r io.Reader) ([]Todo, []Todo, error) {
 	return todos, archs, nil
 }
 
-func SaveCSV(todos []Todo, archs []Todo, r io.Reader) {
+func SaveCSV(todos []Todo, archs []Todo, fp *os.File) {
+	w := bufio.NewWriter(fp)
+	csv_list := append(createTodoCSV(todos), createTodoCSV(archs)...)
+	buf := bytes.NewBufferString("")
+	for i := 0; i < len(csv_list); i++ {
+		buf.WriteString(csv_list[i])
+		// if not last offset, append return '\n' to tail.
+		if i+1 != len(csv_list[i]) {
+			buf.WriteString("\n")
+		}
+	}
+}
 
+func createTodoCSV(todos []Todo) []string {
+	csv := make([]string, 0, len(todos))
+	for i := 0; i < len(todos); i++ {
+		todo := todos[i]
+		buf := bytes.NewBufferString(strconv.Itoa(todo.no))
+		buf.WriteString(",")
+		buf.WriteString(todo.label)
+		buf.WriteString(",")
+		buf.WriteString(todo.title)
+		buf.WriteString(",")
+		if todo.isArchived {
+			buf.WriteString("true")
+		} else {
+			buf.WriteString("false")
+		}
+		csv[i] = buf.String()
+	}
+	return csv
 }
